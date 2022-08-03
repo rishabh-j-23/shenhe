9fd9d0cbda6a0b06684886fe545571d3d28ce8f1
Ryxke <99457863+Ryxke@users.noreply.github.com>
2022-08-03 21:53:26 +0530

    removed weapon refine

--- cogs/genshin/EnkaShinShin.py
+++ cogs/genshin/EnkaShinShin.py
@@ -28,7 +28,7 @@ class EnkaShinShin(commands.Cog):
     
     for character in user.characters:
       if str(character.name) == str(char):
-        des = f"**Character :** {character.name}\n **Friendship :** {character.friendship.level}\n **Weapon** : {character.weapon.nameText} R{character.weapon.rankLevel}"
+        des = f"**Character :** {character.name}\n **Friendship :** {character.friendship.level}\n **Weapon** : {character.weapon.nameText}"
 
         com = character.combat
         """com = char.combat     
