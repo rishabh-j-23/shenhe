c7751108442550f74860694bb29153e1cf6c446e
Ryxke <99457863+Ryxke@users.noreply.github.com>
2022-08-03 20:45:10 +0530

    removed unesessary code

--- cogs/genshin/EnkaShinShin.py
+++ cogs/genshin/EnkaShinShin.py
@@ -99,7 +99,7 @@ class EnkaShinShin(commands.Cog):
   async def player(self, ctx, uid):
     if uid == 'ryxke':
       uid = 712994834
-      
+
     await client.load_lang()
     try:
       user = await client.fetch_user(uid)
@@ -136,28 +136,6 @@ class EnkaShinShin(commands.Cog):
     embed.set_thumbnail(url = ctx.author.avatar_url)
    
     await ctx.send(embed = embed)
-
-  
-  # async def enka(self, ctx, uid, *, char):
-  #   await client.load_lang()
-  #   try:
-  #     user = await client.fetch_user(uid)
-  #   except:
-  #     await ctx.send("Your Character Details are Hidden")
-  #     return
-
-  #   for character in user.characters:
-  #     if str(character.name) == str(char):
-  #       description = f"**{character.name}**\n **Friendship :** {character.friendship.level}\n **Weapon** : {character.weapon.nameText}"
-  #       embed = discord.Embed(title = user.player.nickname, description = description, color=0x06E5F5)
-  #       embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url)
-  #       await ctx.send(embed=embed)
-
-  #       for arti in character.artifacts: 
-  #         embed1 = discord.Embed(title = f"{arti.nameText}({arti.main_stat.value})",description = arti.main_stat.prop, color=0x06E5F5)
-  #         for subs in arti.sub_stats:
-  #           embed1.add_field(name = subs.prop, value = subs.value)
-  #         await ctx.send(embed=embed1)
            
 def setup(bot):
   bot.add_cog(EnkaShinShin(bot))
