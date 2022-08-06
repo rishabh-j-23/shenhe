cadd7a510dcee4921cc4bda0d66e96f5cb5ce8c8
Ryxke <99457863+Ryxke@users.noreply.github.com>
2022-08-06 13:43:27 +0000

    updates in !!enka and new !!artifactcv

--- cogs/changelogs.py
+++ cogs/changelogs.py
@@ -6,7 +6,10 @@ class changelogs(commands.Cog):
     @commands.command(brief = 'Changes in Bot', aliases = ['cl', 'change', 'logs'])
     async def changelogs(self, ctx):
 
-        description = """- added char cons, talents and level in !!enka"""
+        description = """
+        - added command for artifact cv ie !!artifactcv <uid> <char>.
+        - Shows artifact cd, cr and cv in !!enka command
+        - added char cons, talents and level in !!enka"""
         embed = discord.Embed(title = 'Changelogs', description = description, color = 0x06E5F5)
         await ctx.send(embed = embed)
 
