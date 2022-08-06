3a9224bffa51c6c49dcfb1ceb70d37d21f77be3f
Ryxke <99457863+Ryxke@users.noreply.github.com>
2022-08-06 07:54:57 +0530

    Add files via upload

---
+++ cogs/changelogs.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import tasks, commands
+
+class changelogs(commands.Cog):
+
+    @commands.command(brief = 'Changes in Bot', aliases = ['cl', 'change', 'logs'])
+    async def changelogs(self, ctx):
+
+        description = """- added char cons, talents and level in !!enka"""
+        embed = discord.Embed(title = 'Changelogs', description = description, color = 0x06E5F5)
+        await ctx.send(embed = embed)
+
+def setup(bot):
+    bot.add_cog(changelchangelogsogs(bot)d
+    
