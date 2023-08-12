fa714a38a09f5c9228b9372422ef4a179d79b41a
rishabh-j-23 <rjain2311200@gmail.com>
2023-08-12 09:10:49 +0530

    optimised db retrievals

--- modules/database/mongo_genshin.py
+++ modules/database/mongo_genshin.py
@@ -2,9 +2,7 @@ import pymongo
 import os
 
 
-# client = pymongo.MongoClient(os.environ['MONGO_KEY'])
-client = pymongo.MongoClient(
-    f"mongodb+srv://Ryxke:{os.environ['MONGO_PASS']}@shenhedb.tqeaoxl.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://ryxke:{os.environ['MONGO_PASS']}@shenhedb.tqeaoxl.mongodb.net/?retryWrites=true&w=majority")
 db = client['shenheDB']
 uid_col = db['uids']
 
@@ -12,27 +10,18 @@ uid_col = db['uids']
 class MongoGenshin:
 
     def add(self, user: str, uid):
-        uids = {'user': f'{user}', 'uid': f'{uid}'}
-        uid_col.insert_one(uids)
+        user_data = {'user': user, 'uid': uid}
+        uid_col.insert_one(user_data)
 
     def get_uid(self, user: str):
-        u = []
-        for x in uid_col.find({}, {"_id": 0, "user": 1, "uid": 1}):
-            u.append(x)
-
-        for i in range(len(u)):
-            if u[i]['user'] == user:
-                uid = u[i]['uid']
-
-                return int(uid)
+        user_data = uid_col.find_one({'user': user}, {'_id': 0, 'uid': 1})
+        if user_data:
+            return user_data['uid']
+        return None
 
     def get_user(self, uid):
-        u = []
-        for x in uid_col.find({}, {"_id": 0, "user": 1, "uid": 1}):
-            u.append(x)
-
-        for i in range(len(u)):
-            if u[i]['uid'] == str(uid):
-                user = u[i]['user']
-
-                return str(user)
+        user_data = uid_col.find_one(
+            {'uid': str(uid)}, {'_id': 0, 'user': 1})
+        if user_data:
+            return user_data['user']
+        return None
